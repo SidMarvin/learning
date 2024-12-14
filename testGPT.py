cb27067bd2fd1f011fde03a9cd4de70991916d55
SidMarvin <66871853+SidMarvin@users.noreply.github.com>
2024-12-14 18:00:02 +0300

    Update testGPT.py

--- testGPT.py
+++ testGPT.py
@@ -4,27 +4,27 @@
 
 from openai import OpenAI
 
-# Инициализация клиента OpenAI
+#Инициализация
 client = OpenAI(
     api_key="sk-YpYQwVvo4khO6pSxrL5YDS7XvHUGCUaK",
     base_url="https://api.proxyapi.ru/openai/v1",
 )
 
 while True:
-    # Запрос вопроса у пользователя
+    #Запрос
     user_input = input("Введите ваш вопрос (или 'выход' для завершения): ")
     
-    # Условие выхода из цикла
+    #Условие выхода из цикла
     if user_input.lower() == 'выход':
         print("До свидания!")
         break
     
-    # Отправка вопроса в ChatGPT
+    #Отправка вопроса
     chat_completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=[{"role": "user", "content": user_input}]
     )
     
-    # Вывод ответа
-    response = chat_completion.choices[0].message.content  # Исправлено здесь
+    #ответ
+    response = chat_completion.choices[0].message.content
     print("Ответ ChatGPT:", response)
