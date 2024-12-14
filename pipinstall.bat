0b8e287896e87b2651b5d0d42133516790b59acb
SidMarvin <66871853+SidMarvin@users.noreply.github.com>
2024-12-14 18:16:56 +0300

    Create pipinstall.bat

---
+++ pipinstall.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Установка библиотек Python...
+
+python3 -m pip install --upgrade pip
+python -m pip install openai
+python -m pip install tkinter
+python -m pip install имя_библиотеки
+
+echo Установка завершена.
+pause
